112025b797f56a93a6eb3a2ed014f1a09d43ceeb
feavelino <feavelino@gmail.com>
2023-09-15 12:46:42 -0300

    Criação do backend

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.get('/', () => {
     return 'Hello World'
@@ -14,6 +20,8 @@ app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 
 app.register(createTranscriptionRoute)
+
+app.register(generateAiCompletionRoute)
 
 app.listen({
     port: 3333
